0d127f2bb742de5c53217674fb4bbfa3ff0d68a4
Nathaniel Fruchter <nhfruchter@gmail.com>
2017-11-27 15:44:03 -0500

    New source labeling

---
+++ comments_site/core/sources.py
@@ -0,0 +1,67 @@
+
+SOURCE_MAP = {
+    'bot.unprecedented': {
+        'name': '"Unprecedented" bot',
+        'details': 'The original bot that gained fame over the summer.'
+    },
+    'bot.outraged': {
+        'name': '"Outraged" bot'
+    },
+    'bot.wheeler': {
+        'name': 'Taxpayers Protection Alliance 2 / Wheeler',
+        'url': 'http://www.tpaaction.org/',
+        'details': 'This form language pops up on the TPA Action website, but it seems to have spread beyond that petition.'
+    },
+    'form.johnoliver': {
+        'name': 'John Oliver',
+        'url': 'https://gofccyourself.com'
+    },
+    'form.battleforthenet': {
+        'name': 'Battle for the Net',
+        'details': 'This comment listing encompasses a wide array of BftN\'s online campaigns, including the Day of Action.',
+        'url': 'https://www.battleforthenet.com/'
+    },
+    'form.freeourinternet': {
+        'name': "Free Our Internet",
+        'url': 'https://freeourinternet.org/'
+    },
+    'form.reddit.technology': {
+        'name': 'Reddit',
+        'url': 'https://www.reddit.com/r/technology/comments/6894i9/heres_how_to_contact_the_fcc_with_your_thoughts/'
+    },
+    'form.tpa': {
+        'name': 'Taxpayers Protection Alliance',
+        'url': 'https://www.protectingtaxpayers.org/take-action/'
+    },
+    'form.diminished-investment': {
+        'name': '"Diminished Investment" bot',
+    },
+    'form.blog.venturebeat': {
+        'name': 'VentureBeat'
+    },
+    'form.demandprogress': {
+        'name': 'Demand Progress',
+        'url': 'https://act.demandprogress.org/sign/fcc_net_neutrality',
+        'details': 'Demand Progress is an internet-focused activism organization.'
+    },
+    'form.protecting_taxpayers': {
+        'name': 'Taxpayers Progress Alliance 3 / "Concerned taxpayer"',
+        'url': 'https://www.protectingtaxpayers.org/take-action/'
+    },
+    'form.freepress': {
+        'name': 'Free Press',
+        'url': 'http://act.freepress.net/sign/internet_NN_pai/'
+    },
+    'form.signforgood': {
+        'name': 'Sign For Good/Daily Kos',
+        'url': 'https://petitions.signforgood.com/ProtectNetNeutrality/'
+    },
+    'bot.recursive': {
+        'name': 'Clever text-generating bot',
+        'details': 'This bot chains together a large set of phrases in a semi-random way to create unique, realistic looking comments.'
+    },
+    'bot.telecommunications_act': {
+        'name': '"Telecommunications Act" form/bot',
+        'details': 'This comment takes the form of "I am in favor of strong net neutrality under Title II of the Telecommunications Act. Sincerely, <full name>." Some of the addresses associated with these comments are not real.'
+    }
+}
