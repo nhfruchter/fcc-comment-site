8edfc3730c698f7609d5fd2efb00dff0af0ed5f6
Nathaniel Fruchter <nhfruchter@gmail.com>
2017-11-27 15:45:23 -0500

    Theme and static changes

--- comments_site/urls.py
+++ comments_site/urls.py
@@ -1,11 +1,20 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from .core.views import index, browse
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', index),
-    url(r'^browse/?$', browse)
+    url(r'^browse/?$', browse),
+    url(r'^about', 
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    url(r'^credits', 
+        TemplateView.as_view(template_name='credits.html'),
+        name='credits'),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
