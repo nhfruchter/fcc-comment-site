98109333dd8cc6e7079ac51e5b1565793cf99ac9
Nathaniel Fruchter <nhfruchter@gmail.com>
2017-12-30 23:06:32 -0600

    Add more sources

--- comments_site/core/sources.py
+++ comments_site/core/sources.py
@@ -12,9 +12,10 @@ SOURCE_MAP = {
         'url': 'http://www.tpaaction.org/',
         'details': 'This form language pops up on the TPA Action website, but it seems to have spread beyond that petition.'
     },
-    'form.johnoliver': {
+    'johnoliver': {
         'name': 'John Oliver',
-        'url': 'https://gofccyourself.com'
+        'url': 'https://gofccyourself.com',
+        'details': "This is a placeholder for a variety of filings that use phrasing suggested on John Oliver\'s Last Week Tonight episode about net neutrality."
     },
     'form.battleforthenet': {
         'name': 'Battle for the Net',
@@ -24,6 +25,50 @@ SOURCE_MAP = {
     'form.freeourinternet': {
         'name': "Free Our Internet",
         'url': 'https://freeourinternet.org/'
+    },
+    'form.aclu': {
+        'name': "ACLU",
+        'url': "https://action.aclu.org/secure/save-net-neutrality"
+    },
+    'bot.day_of_action': {
+        'name': "NN Day of Action - Unaffiliated",
+        'details': "This comment advertises the Net Neutrality Day of Action in July, but seems to have been pushed by an unaffiliated third party."
+    },
+    'form.freepress2': {
+        'name': "Free Press 2 / Cable and phone",
+        'url': "http://act.freepress.net/letter/internet_fcc_nprm_nn_2017/?t=5&akid=6473.8906785.Y1CsIv"
+    },
+    'bot.internetuser': {
+        'name': 'Internet User / EFF/Unknown - Contested',
+        'url': "https://www.eff.org/deeplinks/2017/06/nlpcs-false-report-diverts-attention-concerns-real-net-neutrality-supporters",
+        'details': "This form comment was created by the EFF, but the EFF has also accused others of borrowing it and creating fake filings."
+    },
+    'bot.telephone_monopoly': {
+        'name': '"Depression-era regulatory framework"'
+    },
+    'bot.dissapear': {
+        'details': "This form filing is notable for its misspellings."
+    },
+    'reddit.technology': {
+        'name': "Reddit /r/technology",
+        'details': "This early form campaign started on the technology subreddit."
+    },
+    'form.techcrunch': {
+        'name': "Techcrunch Campaign"
+    },
+    'form.dearfcc': {
+        'name': '"Dear FCC" Campaign / EFF',
+        'url': 'https://dearfcc.org/',
+        'details': 'Site that allowed you to create a custom-ish comment to file using templates.'
+    },
+    'bot.titleii_takeover': {
+        'name': '"Title II internet takeover"'
+    },
+    'bot.commonsense': {
+        'name': '"commonsense regulatory framework"'
+    },    
+    'bot.rammed': {
+        'name': '"Obama Administration rammed through"'
     },
     'form.reddit.technology': {
         'name': 'Reddit',
